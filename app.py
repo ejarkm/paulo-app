92bcf3a74ef10774422d7862110cb3a38657b7d5
Enrique Alvarez <ejarkm@gmail.com>
2024-08-10 17:54:08 +0000

    Added first app

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from PIL import Image
+import os
+
+
+st.info(
+    """
+At Spaik, we believe that artificial intelligence can be used to improve patient outcomes
+and reduce the cost of healthcare.
+Our mission is to bring the power of AI to healthcare,
+enabling healthcare providers to make better decisions faster.
+
+For more information, please visit our website: [spaik.es](https://spaik.es)
+""")
+
---
+++ pages/1_paulo.py
@@ -0,0 +1,75 @@
+import os
+from joblib import load
+
+import streamlit as st
+
+
+st.markdown(
+    """###  <div style="text-align: left"> Paulo Predictors </div>""",
+    unsafe_allow_html=True,
+)
+st.text("")
+st.info(
+    """
+More text to complete
+"""
+)
+
+st.text("")
+
+with st.expander("Calculator", expanded=True):
+    st.markdown(
+        """ <div style="font-style: Justify"> <div style="font-style: italic"> To complete <div/>"""
+    , unsafe_allow_html=True)
+    st.text("")
+
+    value_das = st.number_input(
+        "**..",
+        min_value=0.0,
+        max_value=100.0,
+        value=0.0,
+        step=0.0001,
+        format="%.4f",
+    )
+    value_pcr = st.number_input(
+        "**......",
+        min_value=0.0,
+        max_value=100.0,
+        value=0.0,
+        step=0.0001,
+        format="%.4f",
+    )
+    value_ril6il2 = st.number_input(
+        "**....",
+        min_value=0.0,
+        max_value=100.0,
+        value=0.0,
+        step=0.0001,
+        format="%.4f",
+    )
+
+    # Del modelo, el label 1 es ser RESPONDEDOR, que en las posiciones de las predicciones es la 0
+    respondedor = 1
+    # Del modelo, el label 2 es ser NO RESPONDEDOR, que en las posiciones de las predicciones es la 1
+    no_respondedor = 2
+
+    model = load(
+        open(os.path.join(os.getcwd(), "models/paulo/model_20240810.pkl"), "rb")
+    )
+
+    input_values = [value_das, value_pcr, value_ril6il2]
+
+if st.button("Start"):
+    if any([True for i in input_values if i == 0]):
+        st.warning(
+            """Warning: One or more values are **zero**.
+            This is allowed, but make sure to check it is valid."""
+        )
+
+    st.markdown("")
+    st.markdown(
+        f"""The probability of being ... is:
+    <br/>
+    <font size="6"> {model.predict_proba([input_values])[0][0]*100:.2f}% </font>""",
+        unsafe_allow_html=True,
+    )
